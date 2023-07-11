df384e3f8a9f2fd4c34b0416a0272bf03b045e4b
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-11 11:07:01 +0000

    Feat: DB for category up and running

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -6,7 +6,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     for_tasks = db.relationship(
-        "Task", backref="category", cascade="all, delete", lazy=True)
+        "Tasks", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         return self.category_name
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 from taskmanager import app, db
 from taskmanager.models import Category, Tasks
 
@@ -6,3 +6,18 @@ from taskmanager.models import Category, Tasks
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def category():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("category"))
+    return render_template("add_category.html")
