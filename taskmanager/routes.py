95f3318356ff0d877b0f9cdfad2f2f29106ad758
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-07-12 12:30:27 +0000

    feat: modal added to the delete function

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -8,12 +8,16 @@ def home():
     return render_template("tasks.html")
 
 # READ
+
+
 @app.route("/categories")
 def category():
     categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
 
 # CREATE
+
+
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
@@ -22,3 +26,25 @@ def add_category():
         db.session.commit()
         return redirect(url_for("category"))
     return render_template("add_category.html")
+
+# UPDATE
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("category"))
+    return render_template("edit_category.html", category=category)
+
+# DELETE
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("category"))
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initialize navbar
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    // Delete Modal (verification)
+    let modal_delete = document.querySelectorAll('.modal');
+    M.Modal.init(modal_delete, inDuration=250);
   });
